6ae7c7311dcf10be187301375812046fd08e1ced
Eli DOE <96851843+elidoegh@users.noreply.github.com>
2022-01-11 16:51:33 -0800

    Update create-dbuser-dms-rds-oracle.sql

--- dms-oracle-scripts/create-dbuser-dms-rds-oracle.sql
+++ dms-oracle-scripts/create-dbuser-dms-rds-oracle.sql
@@ -89,9 +89,9 @@ EXEC rdsadmin.rdsadmin_master_util.create_archivelog_dir;
 EXEC rdsadmin.rdsadmin_master_util.create_onlinelog_dir;
 
 /* Grant read access ONLINELOG_DIR and ARCHIVELOG_DIR if dmsuser is not master user */
-SELECT directory_name, directory_path FROM all_directories WHERE directory_name LIKE ('ARCHIVELOG_DIR_%') OR directory_name LIKE ('ONLINELOG_DIR_%');
---GRANT READ ON DIRECTORY ONLINELOG_DIR TO &1;
---GRANT READ ON DIRECTORY ARCHIVELOG_DIR TO &1;
+SELECT directory_name, directory_path FROM all_directories WHERE directory_name LIKE ('ARCHIVELOG_DIR%') OR directory_name LIKE ('ONLINELOG_DIR%');
+GRANT READ ON DIRECTORY ONLINELOG_DIR TO &1;
+GRANT READ ON DIRECTORY ARCHIVELOG_DIR TO &1;
 
 /* Configure adequate retention for archived redo log*/
 EXEC rdsadmin.rdsadmin_util.set_configuration('archivelog retention hours',24);
